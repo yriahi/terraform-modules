adcd5f9a096570943fa9d238cf0a548956c06b8c
Youssef Riahi <youssef.riahi@mass.gov>
2019-03-29 22:21:29 -0400

    vars file update

--- static-site/example/variables.tf
+++ static-site/example/variables.tf
@@ -3,3 +3,10 @@ variable "name_prefix" {
   type = "string"
   description = "The name prefix to use for all created resources."
 }
+
+# resource tagging
+variable "tags" {
+  type = "map"
+  description = "A map of tags to apply to created resources."
+  default = {}
+}
