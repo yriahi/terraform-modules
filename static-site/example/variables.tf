130b6318ea73e0c5ce896f050186b2ebfe6d5848
Youssef Riahi <youssef.riahi@mass.gov>
2019-03-29 22:21:01 -0400

    variables file

---
+++ static-site/example/variables.tf
@@ -0,0 +1,5 @@
+# useful if you have a naming convention for naming your resources.
+variable "name_prefix" {
+  type = "string"
+  description = "The name prefix to use for all created resources."
+}
