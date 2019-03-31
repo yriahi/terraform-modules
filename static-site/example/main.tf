b2319b2085cfbd642e41f5856682151290a23db9
Youssef Riahi <youssef.riahi@mass.gov>
2019-03-31 12:26:49 -0400

    rename example instance name

--- static-site/example/main.tf
+++ static-site/example/main.tf
@@ -1,5 +1,5 @@
 
-module "my_new_cool_static_site" {
+module "my_static_site" {
   source = "github.com/yriahi/terraform-modules//static-site?ref=0.2.0"
   domain_name = "${var.domain_name}"
   origin_id = "${var.origin_id}"
