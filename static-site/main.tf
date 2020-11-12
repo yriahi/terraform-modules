2b1da4a2725a488bd507de36909ce5a47fa36282
Youssef Riahi <youssef.riahi@mass.gov>
2020-11-12 14:54:36 -0500

    fix identifier name

--- static-site/main.tf
+++ static-site/main.tf
@@ -90,7 +90,7 @@ resource "aws_acm_certificate" "default" {
 // create the DNS entry in th relevant zone
 resource "aws_route53_record" "verification" {
   for_each = {
-    for dvo in aws_acm_certificate.existing.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
